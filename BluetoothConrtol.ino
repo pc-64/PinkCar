0da92892583ddc238d236834db3c01b40ad1bdea
pc-64 <ainpk12@gmail.com>
2025-03-29 18:42:10 +0300

    Create BluetoothConrtol.ino

---
+++ BluetoothConrtol.ino
@@ -0,0 +1,26 @@
+#include <SoftwareSerial.h>
+
+const byte lPin = A0;
+const byte rPin = A1;
+
+SoftwareSerial radio(2, 3);
+
+void setup()
+{
+   radio.begin(9600);
+}
+
+void loop()
+{
+   static unsigned long timer = 0;
+   unsigned long interval = 250;
+   if (millis() - timer >= interval)
+   {
+      timer = millis();
+      int lValue = analogRead(lPin);
+      int rValue = analogRead(rPin);
+      char buffer[15];
+      snprintf(buffer, 14, "%d,%d", lValue, rValue);
+      radio.println(buffer);
+   }
+}
